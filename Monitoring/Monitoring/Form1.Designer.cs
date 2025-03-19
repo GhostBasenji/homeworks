9a5304dcc06d37b89a683f49fe450e6c7d3aed1f
Andrey P <petr.and.vic@gmail.com>
2025-03-19 23:54:13 +0400

    Домашнее задание #015 - Координаты курсора мыши относительно экрана и элемента управления

---
+++ Monitoring/Monitoring/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Monitoring
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(354, 259);
+            listBox1.TabIndex = 0;
+            listBox1.MouseEnter += listBox1_MouseEnter;
+            listBox1.MouseLeave += listBox1_MouseLeave;
+            listBox1.MouseMove += listBox1_MouseMove;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 290);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(171, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(195, 290);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(171, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(378, 325);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}

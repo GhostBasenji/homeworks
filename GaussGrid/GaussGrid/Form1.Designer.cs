24d391959974fa6da6ede11bd08c00b498e87223
Andrey P <petr.and.vic@gmail.com>
2025-07-24 14:38:35 +0400

    ДЗ #53

---
+++ GaussGrid/GaussGrid/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace GaussGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(193, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Введите количество неизвестных:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(211, 11);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(157, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 40);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(586, 286);
+            dataGridView1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(115, 332);
+            button1.Name = "button1";
+            button1.Size = new Size(379, 37);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(610, 373);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private DataGridView dataGridView1;
+        private Button button1;
+    }
+}
